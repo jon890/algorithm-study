be3b6cf9bf059ff321f1a157f4122f176dcf87a0
jon890 <jon89071@gmail.com>
2021-09-30 23:54:47 +0900

    피보나치 수 풀이

---
+++ src/programmers/level2/피보나치_수.java
@@ -0,0 +1,14 @@
+package programmers.level2;
+
+public class 피보나치_수 {
+
+    private static int[] dp = new int[100001];
+
+    public int solution(int n) {
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp [i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
+        return dp[n];
+    }
+}

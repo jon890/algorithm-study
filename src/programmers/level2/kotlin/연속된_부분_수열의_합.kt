a0a41a9c8c1e0146843b6995cfffe99eec70bcc0
BiFoS <jon89071@gmail.com>
2024-04-23 14:29:51 +0900

    :art: 연속된 부분 수열의 합 개선

--- src/programmers/level2/kotlin/연속된_부분_수열의_합.kt
+++ src/programmers/level2/kotlin/연속된_부분_수열의_합.kt
@@ -6,44 +6,40 @@ package programmers.level2.kotlin
  *
  * 첫 시도 => 41.2점
  * 두 번째 시도 => 44.1점
+ * 세 번째 시도 => 2차원 배열의 메모리 초과를 어떻게 해결할까?
+ * 네 번째 시도 => 이전 합을 1차원 배열로 처리해도 무방하다, 하지만 시간 초과 여전히 발생
  */
 class 연속된_부분_수열의_합 {
     fun solution(sequence: IntArray, k: Int): IntArray {
-        val map = mutableMapOf<String, String>()
+        val prevSum = IntArray(sequence.size) { 0 }
         for (i in sequence.indices) {
-            map["${i}_${i}"] = "${sequence[i]}_${1}_${i}"
+            prevSum[i] = sequence[i]
             if (sequence[i] == k) {
                 return intArrayOf(i, i)
             }
         }
 
         for (size in 2..sequence.size) {
-            for (i in 0..sequence.size - size) {
-                val end = i + size - 1
-                val data = map["${i}_${end - 1}"]?.split("_") ?: throw IllegalStateException()
-
-                val sum = data[0].toInt() + sequence[end]
-                map["${i}_${end}"] = "${sum}_${size}_${i}"
-
+            for (start in 0..sequence.size - size) {
+                val end = start + size - 1
+                val sum = prevSum[start] + sequence[end]
+                prevSum[start] = sum
                 if (sum == k) {
-                    return intArrayOf(i, end)
+                    return intArrayOf(start, end)
                 }
             }
         }
 
-        return intArrayOf(0, 0)
+        throw IllegalStateException("정답을 발견하지 못했습니다")
     }
 }
 
 fun main() {
     val sol = 연속된_부분_수열의_합()
 
-//    val ret1 = sol.solution(sequence = intArrayOf(1, 2, 3, 4, 5), k = 7)
-//    if (ret1[0] != 2 || ret1[1] != 3) {
-//        println("$ret1[0] $ret1[1]")
-//        println("정답이 아님")
-//    }
-
-    val ret2 = sol.solution(intArrayOf(1, 1, 1, 2, 3, 4, 5), 5)
+    val ret1 = sol.solution(sequence = intArrayOf(1, 2, 3, 4, 5), k = 7)
+    if (ret1[0] != 2 || ret1[1] != 3) {
+        println("정답이 아님")
+    }
 }
 
